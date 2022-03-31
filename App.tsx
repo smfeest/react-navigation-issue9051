81dd1bbd0358bcaffb3dba40caf57879e8fd42a5
Stephen Feest <stephen.feest@futurelearn.com>
2022-03-31 11:22:17 +0100

    Min repro of warning on closing modal with gestures enabled
    To reproduce:
    
    1. Run the application on Android
    
       ```
       yarn install
       yarn android
       ```
    
    2. Press 'Open modal'
    
    3. Press 'Close modal'
    
    The follow warning is then raised:
    
    ```
     ERROR  Warning: Can't perform a React state update on an unmounted component. This is a no-op, but it indicates a memory leak in your application. To fix, cancel all subscriptions and asynchronous tasks in the componentWillUnmount method.
        in Card (at CardContainer.tsx:219)
        in CardContainer (at CardStack.tsx:654)
        in RNSScreen (at createAnimatedComponent.js:211)
        in AnimatedComponent (at createAnimatedComponent.js:264)
        in AnimatedComponentWrapper (at src/index.native.tsx:281)
        in MaybeFreeze (at src/index.native.tsx:280)
        in Screen (at Screens.tsx:37)
        in MaybeScreen (at CardStack.tsx:647)
    ```
    
    The error does not occur if you close the modal using the gesture or
    close button in the header.

---
+++ App.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import {NavigationContainer, NavigationProp} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {Pressable, Text} from 'react-native';
+
+const ParentScreen = ({
+  navigation: {navigate},
+}: {
+  navigation: NavigationProp<NavigationParams>;
+}) => (
+  <Pressable onPress={() => navigate('Child')}>
+    <Text>Open modal</Text>
+  </Pressable>
+);
+
+const ChildScreen = ({
+  navigation: {goBack},
+}: {
+  navigation: NavigationProp<NavigationParams>;
+}) => (
+  <Pressable onPress={goBack}>
+    <Text>Close</Text>
+  </Pressable>
+);
+
+type NavigationParams = {
+  Parent: undefined;
+  Child: undefined;
+};
+
+const Stack = createStackNavigator<NavigationParams>();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Parent" component={ParentScreen} />
+        <Stack.Screen
+          name="Child"
+          component={ChildScreen}
+          options={{
+            gestureEnabled: true,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
